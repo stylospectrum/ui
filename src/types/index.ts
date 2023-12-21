f3098f387900caba3188f8772dde8e3d369ed9d1
Your Name <you@example.com>
2023-12-21 11:18:59 +0700

    chore(types): add

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type {default as IButton} from '../button';
+export type {default as IIcon} from '../icon';
+export type {default as IInput} from '../input';
+export type {default as ILabel} from '../label';
+export type {default as ICheckbox} from '../checkbox';
+export type {default as IToast} from '../toast';
+export type {default as IMessageStrip} from '../message-strip';
+export type {default as IForm} from '../form';
+export type {default as IFormItem} from '../form';
+export type {default as ILink} from '../link';
