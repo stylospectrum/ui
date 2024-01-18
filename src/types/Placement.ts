df6dee85d1a2bc186a6e6012542ff2a5e3f25e07
Your Name <you@example.com>
2024-01-18 17:22:47 +0700

    feat(Popover): add Placement.Left

--- src/types/Placement.ts
+++ src/types/Placement.ts
@@ -1,5 +1,31 @@
+/**
+ * Popup placement types.
+ *
+ * @public
+ */
 enum Placement {
+  /**
+   * Popup will be placed at the left side of the reference element.
+   * @public
+   */
+  Left = 'Left',
+
+  /**
+   * Popup will be placed at the right side of the reference element.
+   * @public
+   */
+  Right = 'Right',
+
+  /**
+   * Popup will be placed at the top of the reference element.
+   * @public
+   */
   Top = 'Top',
+
+  /**
+   * Popup will be placed at the bottom of the reference element.
+   * @public
+   */
   Bottom = 'Bottom',
 }
 
