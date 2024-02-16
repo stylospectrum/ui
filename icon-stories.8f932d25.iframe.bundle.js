41a6ef46f5c45f5fd0e13aef44c04d0bf632de14
stylospectrum <stylospectrum@users.noreply.github.com>
2024-02-16 03:36:37 +0000

    Deploying to gh-pages from @ stylospectrum/ui@0a954d42865bf6b6d52bd5957fdf1bc655f0e6cd 🚀

--- icon-stories.8f932d25.iframe.bundle.js
+++
@@ -1,7 +0,0 @@
-"use strict";(self.webpackChunk_stylospectrum_ui=self.webpackChunk_stylospectrum_ui||[]).push([[92],{"./src/icon/stories.ts":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Basic:()=>Basic,default:()=>__WEBPACK_DEFAULT_EXPORT__});var lit__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__("./node_modules/lit/index.js"),lit_directives_if_defined_js__WEBPACK_IMPORTED_MODULE_2__=(__webpack_require__("./src/icon/index.ts"),__webpack_require__("./node_modules/lit/directives/if-defined.js"));const __WEBPACK_DEFAULT_EXPORT__={title:"Icon",component:"stylospectrum-icon",tags:["autodocs"]},Basic=(args=>{let iconName="";try{iconName=__webpack_require__("./src/icon/data sync recursive ^\\.\\/.*$")(`./${args.name}`).default}catch(err){console.log(err)}return lit__WEBPACK_IMPORTED_MODULE_0__.kP`
-    <stylospectrum-icon
-      name="${(0,lit_directives_if_defined_js__WEBPACK_IMPORTED_MODULE_2__.u)(iconName)}"
-      style="color: white"
-    ></stylospectrum-icon>
-  `}).bind({});Basic.args={name:"decline"}}}]);
-//# sourceMappingURL=icon-stories.8f932d25.iframe.bundle.js.map
